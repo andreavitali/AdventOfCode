65c000529b2f7d25e5448bf125844f31e8a0e366
Andrea Vitali <andrea.vitali@anticipay.it>
2024-01-13 16:30:14 +0100

    Completed Day21 (Part 1)

---
+++ 2023/Day21/Day21.cs
@@ -0,0 +1,70 @@
+﻿namespace AdventOfCode2023;
+
+public partial class Day21 : IDay
+{
+    public enum Dir { Up, Down, Left, Right }
+    record Point(long X, long Y);
+
+    private IEnumerable<Point> ParseInput(string input)
+    {
+        int rowIdx = 0;
+        foreach (string row in input.Split(Environment.NewLine))
+        {
+            var colIdx = 0;
+            foreach (var col in row)
+            {
+                if (col != '#') yield return new Point(rowIdx, colIdx);
+                colIdx++;
+            }
+            rowIdx++;
+        }
+    }
+
+    long Walk(HashSet<Point> map, int numOfSteps = 64)
+    {
+        var mapSize = Convert.ToInt32(map.MaxBy(p => p.Y)?.Y / 2);
+        var startPoint = new Point(mapSize, mapSize);
+        var positions = new HashSet<Point> { startPoint };
+        for (int i = 0; i < numOfSteps; i++)
+        {
+            var temp = new HashSet<Point>();
+            foreach (Point point in positions)
+            {
+                foreach (Dir dir in Enum.GetValues(typeof(Dir)))
+                {
+                    Point? newPoint = dir switch
+                    {
+                        Dir.Up => new(point.X - 1, point.Y),
+                        Dir.Down => new(point.X + 1, point.Y),
+                        Dir.Left => new(point.X, point.Y - 1),
+                        Dir.Right => new(point.X, point.Y + 1),
+                        _ => null
+                    };
+
+                    if (newPoint != null)
+                    {
+                        Point transposedPoint = new(Mod(newPoint.X, mapSize), Mod(newPoint.Y, mapSize));
+                        if (map.Contains(transposedPoint)) temp.Add(newPoint);
+                    }
+                }
+            }
+
+            positions = temp;
+        }
+        return positions.Count;
+    }
+
+    long Mod(long n, int m) => ((n % m) + m) % m;
+
+    public long Part1(string input)
+    {
+        var map = ParseInput(input).ToHashSet();
+        return Walk(map, 64);
+    }
+
+    public long Part2(string input)
+    {
+        var map = ParseInput(input).ToHashSet();
+        return Walk(map, 100);
+    }
+}
